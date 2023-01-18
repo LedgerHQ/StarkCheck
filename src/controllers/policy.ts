22538ad24ccc29dea95a1247ab701c7d1717c5e9
Thomas Brillard <thomas.brillard@ledger.fr>
2023-01-18 13:09:30 +0100

    get policy from event logs

--- src/controllers/policy.ts
+++ src/controllers/policy.ts
@@ -3,20 +3,18 @@ import { InvocationsDetailsWithNonce } from "starknet";
 
 import policyService from '../services/policy';
 
-import { Policy } from '../types/policy';
-
 // adding a post
 const verifyPolicy = async (req: Request, res: Response, next: NextFunction) => {
     // get the data from req.body
-    let policy: Policy[] = req.body.policy;
     let account: string = req.body.account;
     let transaction: InvocationsDetailsWithNonce = req.body.transaction;
     try {
-        const policyRes: any = await policyService.verifyPolicy(account, policy, transaction);
+        const policyRes: any = await policyService.verifyPolicy(account, transaction);
         return res.status(200).json({
             message: policyRes
         });
     } catch (error) {
+        console.log(error)
         return res.status(400).json({
             message: error
         });
