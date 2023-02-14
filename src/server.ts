6cff8200574775cdd0317248d87086a29b8a589b
Thomas Brillard <thomas.brillard@ledger.fr>
2023-02-14 17:44:03 +0100

    Update src/server.ts
    Co-authored-by: qdqd <qdqdqdqdqd@protonmail.com>

--- src/server.ts
+++ src/server.ts
@@ -38,6 +38,7 @@ router.use((req, res, next) => {
 });
 
 /** Server */
-const httpServer = http.createServer(router);
 const PORT: any = process.env.PORT ?? 6060;
-httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+router.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}`)
+});
