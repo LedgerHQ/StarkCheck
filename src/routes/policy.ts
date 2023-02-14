b8beddde6db17cbbf54d1fd6b662c56b07cef69f
Thomas Brillard <thomas.brillard@ledger.fr>
2023-02-14 17:29:07 +0100

    Update src/routes/policy.ts
    Co-authored-by: qdqd <qdqdqdqdqd@protonmail.com>

--- src/routes/policy.ts
+++ src/routes/policy.ts
@@ -5,4 +5,4 @@ const router = express.Router();
 router.post('/starkchecks/verify', controller.verifyPolicy);
 router.post('/starkchecks/encodePolicy', controller.encodePolicy);
 
-export = router;
+export { router };
