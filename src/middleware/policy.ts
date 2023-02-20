adb7aee30b96c9f9396335466f62095cb250c85d
Thomas Brillard <thomas.brillard@ledger.fr>
2023-02-20 14:13:20 +0100

    policy middleware to ensure policy is correctly encoded

---
+++ src/middleware/policy.ts
@@ -0,0 +1,35 @@
+import { Policy } from "../types/policy"
+import { Request, Response, NextFunction } from 'express';
+
+/**
+ * Middleware that ensure policy is an array of Policy
+ */
+export const policyCheckMiddleware = () => (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (!req.body.policy) {
+        res.status(400).json({
+            message: "policy is missing"
+        });
+    }
+    else if (!Array.isArray(req.body.policy)) {
+        res.status(400).json({
+            message: "policy is not an array"
+        });
+    } else {
+        const policyFormatOk = !req.body.policy.reduce( (res: boolean, pol: any) => {
+            const key = Object.keys(pol);
+            const ok = key.includes("address") && (key.includes("ids") && Array.isArray(pol.ids) || key.includes("amount"))
+            return res || !ok;
+        }, false)
+        if (policyFormatOk) {
+            next()
+        } else {
+            res.status(400).json({
+                message: "policy malformed"
+            });
+        }
+    }
+}
