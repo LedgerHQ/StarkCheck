39368839753a04cc351c7686e4c94b075b4fefd7
Thomas Brillard <thomas.brillard@ledger.fr>
2023-02-27 11:48:21 +0100

    split app and server for test + renamed components

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express, { Express } from 'express';
+import morgan from 'morgan';
+import router from './routes/policy';
+
+const app: Express = express();
+
+/** Logging */
+app.use(morgan('dev'));
+/** Parse the request */
+app.use(express.urlencoded({ extended: false }));
+/** Takes care of JSON data */
+app.use(express.json());
+
+/** RULES OF OUR API */
+app.use((req, res, next) => {
+  // set the CORS policy
+  res.header('Access-Control-Allow-Origin', '*');
+  // set the CORS headers
+  res.header(
+    'Access-Control-Allow-Headers',
+    'origin, X-Requested-With,Content-Type,Accept, Authorization'
+  );
+  // set the CORS method headers
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+/** Routes */
+app.use('/', router);
+
+/** Error handling */
+app.use((_, res) => {
+  const error = new Error('not found');
+  return res.status(404).json({
+    message: error.message,
+  });
+});
+
+export default app;
