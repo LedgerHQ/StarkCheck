32fabd71d0fd2a88379fb63755609774f42b68e4
Thomas Brillard <thomas.brillard@ledger.fr>
2023-06-08 16:06:33 +0200

    lint

--- src/services/signer.ts
+++ src/services/signer.ts
@@ -4,11 +4,13 @@ import {
   InvocationsDetailsWithNonce,
   Invocation,
   hash,
-  stark,
   Signature,
+  RawCalldata,
 } from 'starknet';
 
-const starkCheckSignerPk = process.env.STARKCHECK_PK || '';
+const starkCheckSignerPk =
+  process.env.STARKCHECK_PK ||
+  '0x019800ea6a9a73f94aee6a3d2edf018fc770443e90c7ba121e8303ec6b349279';
 
 /**
  * Recompute the transacionHash and sign it to proove StarkCheck verified the transaction
@@ -22,9 +24,8 @@ function signTransactionHash(
   const txHash = hash.calculateTransactionHash(
     tx.contractAddress,
     tx.version || 1,
-    // @ts-ignore
-    tx.calldata || [],
-    tx.maxFee,
+    tx.calldata as RawCalldata,
+    tx.maxFee || '2000000000',
     chainId,
     tx.nonce
   );
